9ddd43e9e8dda7b8aee72e91f636166f9adea561
rlynch3456 <rlynch3456@yahoo.com>
2017-02-17 02:13:13 +0000

    Initial commit of area.c example program

---
+++ c/area.c
@@ -0,0 +1,88 @@
+/*
+    area.c
+
+    Author: Rich Lynch
+    For the programming merit badge class.
+  
+    Released to the public under the MIT license.
+
+    The purpose of this program is to demonstrate simple functions in C.
+    
+    The student is expected to add a function to calculate the area of a
+    rectangle, check for valid input and write simple test cases.
+    
+*/
+
+#include <stdio.h>  // we need to include this file to print to console
+
+/*  ToDo: create a function here to calculate the area of
+    a rectangle.  This function will take two integers as parameters
+    and return the area.
+    
+    The function should check for invalid input.  Can a rectangle
+    have a negative or zero area?  Hmmm, I do not think so.  The
+    function should check for incorect input values and return error.
+*/
+
+
+/*
+    Let's make some test cases to make sure that our function is working
+    correctly.
+    
+    Assume that our function is called Area.
+    We know that the area of a 2x3 rectangle shoudl be 6, so we could
+    have a test like this
+    
+    int area;
+    
+    area = Area(2,3);
+    
+    Now check the result
+    
+    if(Area != 6)
+        printf("Error\n");
+        
+    What other tests could we do?  Could the area be zero or a negative number?
+    
+    The test function should return the results of the test.  Typically this
+    would be zero for no error, and something other than zero to indiciate failure.
+
+
+*/
+
+int testArea(void)
+{
+    int ERROR = 0;
+    
+    
+    return ERROR;
+}
+
+
+/* main function is where the program begins its execution */
+int main(int args, char * argv[])
+{
+    
+    // define integer variables for height, width and area
+    
+    
+    
+    
+    // call the area function
+    
+    
+    
+    
+    // print the area
+    
+    
+    
+    
+    // call the test function and report the results
+    
+    
+    
+    
+    return 0;
+    
+}
