402cb979e0f01e896164ee07d78d234aefc42052
rlynch3456 <rlynch3456@yahoo.com>
2018-01-22 02:53:58 +0000

    Initial commit of lua files

---
+++ lua/helloworld.lua
@@ -0,0 +1,16 @@
+#!/usr/local/bin/lua
+
+--[[ 	helloworld.lua
+
+	Author: Rich Lynch
+	For the programming merit badge.
+
+	Released to the public under the MIT license
+
+	The purpose of this program is to demonstrate the most basic of any program.
+
+	helloworld is conisdered to be the starting program when learning any new language.
+	The program simply displays "Hello World" on the display screen.
+]]--
+
+print "Hello World"
